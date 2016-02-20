5605e9466f1c4a7709992830b2982072fdd2dd80
Stig Henriksen <mail@stighenriksen.com>
2016-02-20 13:12:14 +0100

    Give sudo access to system-ctl to jenkins for now.

--- machines/linode/configuration.nix
+++ machines/linode/configuration.nix
@@ -92,6 +92,7 @@ in
   security.sudo.extraConfig = ''
     jenkins ALL = NOPASSWD: /run/current-system/sw/bin/nixos-rebuild
     jenkins ALL = NOPASSWD: /run/current-system/sw/bin/nix-env
+    jenkins ALL = NOPASSWD: /run/current-system/sw/bin/system-ctl
   '';
   services.haproxy.enable = true;
 
